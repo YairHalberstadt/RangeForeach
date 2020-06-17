9f0ce529cb095e0d02afb25135a871c677a9e05a
yair halberstadt <yairhalberstadt@gmail.com>
2020-06-17 17:01:54 +0300

    Initial implementation of extension GetEnumerator for System.Range

---
+++ RangeForeach.Tests.Unit/RangeExtensionsTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Xunit;
+
+namespace RangeForeach.Tests.Unit
+{
+    public class RangeExtensionsTests
+    {
+        [Fact]
+        public void CanForeachOnRange()
+        {
+            var items = new List<int>();
+            foreach(var i in 7..13)
+            {
+                items.Add(i);
+            }
+
+            Assert.Equal(new[] { 7, 8, 9, 10, 11, 12 }, items);
+        }
+
+        [Fact]
+        public void ErrorIfStartIsFromIndex()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in ^7..13) { } });
+        }
+
+        [Fact]
+        public void ErrorIfEndIsFromIndex()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in 7..^13) { } });
+        }
+
+        [Fact]
+        public void ErrorIfStartAndEndIsFromIndex()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in ^7..^13) { } });
+        }
+
+        [Fact]
+        public void ErrorIfStartIsNegative()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in -7..13) { } });
+        }
+
+        [Fact]
+        public void ErrorIfEndIsNegative()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in 7..-13) { } });
+        }
+
+        [Fact]
+        public void ErrorIfStartAndEndIsNegative()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in -7..-6) { } });
+        }
+
+        [Fact]
+        public void ErrorIfStartIsGreaterThanEnd()
+        {
+            var items = new List<int>();
+            Assert.Throws<ArgumentException>(() => { foreach (var i in 1..0) { } });
+        }
+
+        [Fact]
+        public void StartCanBeSameAsEnd()
+        {
+            var items = new List<int>();
+            foreach (var i in 0..0)
+            {
+                items.Add(i);
+            }
+            Assert.Empty(items);
+        }
+    }
+}
---
+++ RangeForeach/RangeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public static class RangeExtensions
+{
+    public struct RangeEnumerator
+    {
+        public RangeEnumerator(int start, int end) => (Current, _end) = (start - 1, end);
+        public int Current { get; private set; }
+        private int _end;
+        public bool MoveNext() => ++Current < _end;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static RangeEnumerator GetEnumerator(this Range range)
+    {
+        if (range.Start.IsFromEnd || range.End.IsFromEnd)
+            ThrowIsFromEnd();
+
+        if (range.Start.Value > range.End.Value)
+            ThrowStartIsGreaterThanEnd();
+
+        return new RangeEnumerator(range.Start.Value, range.End.Value);
+
+        static void ThrowIsFromEnd() => throw new ArgumentException("range start and end must not be from end");
+        static void ThrowStartIsGreaterThanEnd() => throw new ArgumentException("start is greater than end");
+    }
+}
